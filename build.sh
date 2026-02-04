0324a23926c2fb93b721844e3263cf62f0d4e236
ANURAG IT HOD <hod.inf@anurag.ac.in>
2026-02-04 10:15:18 +0530

    Fix build.sh for Render

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "Using Python:"
 python --version
 
 pip install --upgrade pip setuptools wheel
-
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
