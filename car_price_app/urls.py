7a6089de61faa271694635879ce7254ebda3e9a2
ANURAG IT HOD <hod.inf@anurag.ac.in>
2026-06-10 22:07:16 +0530

    Cleanup UML Diagrams: enforce A4 print layout, remove state chart and duplicates

--- car_price_app/urls.py
+++ car_price_app/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 
-from .views import apriori_execution, execution_overview, execution_step, maruti_prices, registration, research_paper
+from .views import apriori_execution, execution_overview, execution_step, registration
 
 urlpatterns = [
     path('', registration, name='registration'),
     path('execution/', execution_overview, name='execution-overview'),
     path('execution/<slug:step_slug>/', execution_step, name='execution-step'),
     path('apriori/', apriori_execution, name='apriori-execution'),
-    path('maruti-prices/', maruti_prices, name='maruti-prices'),
-    path('research-paper/', research_paper, name='research-paper'),
 ]
