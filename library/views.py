28191e181bed3bbe4f27ef719bb06c68d1a4fbfc
ANURAG IT HOD <hod.inf@anurag.ac.in>
2026-07-09 07:50:19 +0530

    Fix missing BookRecommendation import causing 500 on aeclibrary home

--- library/views.py
+++ library/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from .models import Book,Author,Issue,Fine
+from .models import Book,Author,Issue,Fine,BookRecommendation
 from student.models import Student
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
