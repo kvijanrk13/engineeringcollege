f14a457da373b9d3fc8b5de4239d057b2d171797
ANURAG IT HOD <hod.inf@anurag.ac.in>
2026-07-16 08:10:45 +0530

    Fix NameError: import LibraryStat in views

--- library/views.py
+++ library/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from .models import Book,Author,Issue,Fine,BookRecommendation
+from .models import Book,Author,Issue,Fine,BookRecommendation,LibraryStat
 from student.models import Student
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
