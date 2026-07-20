86aa01425c39875d89f8a46024d59472fbaa0eb3
ANURAG IT HOD <hod.inf@anurag.ac.in>
2026-07-20 10:37:25 +0530

    feat: create CSE student accounts 25C11A6601-6665

---
+++ student/migrations/0002_create_cse_students.py
@@ -0,0 +1,49 @@
+from django.db import migrations
+from django.contrib.auth.models import User
+from student.models import Department, Student
+
+
+def create_cse_students(apps, schema_editor):
+    User = apps.get_model('auth', 'User')
+    Department = apps.get_model('student', 'Department')
+    Student = apps.get_model('student', 'Student')
+
+    dept, _ = Department.objects.get_or_create(name='CSE')
+
+    for i in range(6601, 6666):
+        username = f'25C11A{i}'
+        password = str(i)
+
+        user, created = User.objects.get_or_create(
+            username=username,
+            defaults={
+                'email': f'{username}@anurag.ac.in',
+                'first_name': 'Student',
+                'last_name': str(i),
+                'is_staff': False,
+                'is_active': True,
+            }
+        )
+
+        if created:
+            user.set_password(password)
+            user.save()
+
+        Student.objects.get_or_create(
+            student_id=user,
+            defaults={
+                'first_name': 'Student',
+                'last_name': str(i),
+                'department': dept,
+            }
+        )
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('student', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_cse_students),
+    ]
