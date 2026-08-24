51318e71005dd74b81d8f5d775e58fc7e612d540
ANURAG IT HOD <hod.inf@anurag.ac.in>
2026-08-24 21:15:23 +0530

    Align MOOCS questions with UGC NET syllabus

--- engineeringcollege/tests.py
+++ engineeringcollege/tests.py
@@ -9,3 +9,5 @@ class MoocsPageTests(SimpleTestCase):
         self.assertContains(response, "Computer Science Mock Examination")
         self.assertContains(response, "Question palette")
         self.assertContains(response, "/static/moocs/moocs.js")
+        self.assertContains(response, "/static/moocs/syllabus_questions.js")
+        self.assertContains(response, "/static/moocs/feedback.css")
